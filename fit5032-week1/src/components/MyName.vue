a44e75330d2567d126e6837ab855048deb10c04d
zche0345 <zche0345@student.monash.edu>
2025-08-08 21:28:13 +1000

    Added week1 lab tasks

---
+++ fit5032-week1/src/components/MyName.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <p>My name is Zhuoyi Chen(35262427)</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyName'
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 1.5em;
+  color: #42b983;
+  font-weight: bold;
+}
+</style>
